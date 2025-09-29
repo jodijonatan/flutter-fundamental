0b4028a0e36caf5191e50987d23f3acc8b193a5a
Jodi Jonatan <jdi102350@gmail.com>
2025-09-29 14:36:06 +0700

    belajar menambah gambar

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,25 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'SMK Telkom 1 Medan',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.blue,
+          title: Text('Image Widget Flutter'),
+          backgroundColor: Colors.blue[400],
         ),
-        backgroundColor: Colors.white, // Pindahkan properti di sini
         body: Center(
-          child: Text(
-            "Selamat datang Jodi Jonatan Karo Karo",
-            style: TextStyle(color: Colors.black, fontSize: 18.0),
+          child: Container(
+            width: 400,
+            height: 200,
+            color: Colors.blue[200],
+            // child: Image.asset('images/awan-pintar.png', fit: BoxFit.cover)
+            // ,
+            child: Image.network(
+              "https://picsum.photos/seed/picsum/200/300",
+              fit: BoxFit.fill,
+            ),
           ),
         ),
       ),
